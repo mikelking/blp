51814516f1d870ec25c49947429f012254f2dddb
input - process - output <forestbaker@users.noreply.github.com>
2016-01-17 17:35:54 -0800

    added exit 0 at bottom and cleaned up spacing

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,22 +8,22 @@ BinPath=/usr/local/bin
 
 if [ -e /usr/local/lib/blp/std.blp ];
 	then
-	. /usr/local/lib/blp/std.blp
-	if [ ${Version} = ${blpVer}  ];
-	then
-		echo "ERROR: blp version ${blpVer} is already installed. Abort operation!"
-		exit 1
-	fi
+		. /usr/local/lib/blp/std.blp
+		if [ ${Version} = ${blpVer}  ];
+			then
+				echo "ERROR: blp version ${blpVer} is already installed. Abort operation!"
+				exit 1
+		fi
 fi
 
 	./README
 
 	echo "Installing ${App} version: ${Version}"
-
 	echo "Installing appropriate libraries."
+
 	if [ ! -e /usr/local/lib/blp ];
-	then
-		mkdir -p "${blpPath}/blp"
+		then
+			mkdir -p "${blpPath}/blp"
 	fi
 	
 	cp  *.blp "${blpPath}/blp/"
@@ -34,4 +34,4 @@ fi
 
 	./ReleaseNotes
 
-
+exit 0
